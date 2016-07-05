5481a984cbcf21c086778d8e47803e85548780a9
zckeyser <zckeyser@gmail.com>
2016-07-04 21:04:06 -0400

    added coercer tests

---
+++ Test/TypeCoercerTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArgMap;
+
+namespace Test
+{
+    [TestClass]
+    public class TypeCoercerTest
+    {
+        private TypeCoercer coercer;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            coercer = new TypeCoercer();
+        }
+
+        [TestMethod]
+        public void TypeCoercer_Coerce_CoercesBool()
+        {
+            string toCoerce = "true";
+
+            var result = coercer.Coerce(toCoerce);
+
+            Assert.IsInstanceOfType(result, typeof(bool));
+        }
+
+        [TestMethod]
+        public void TypeCoercer_Coerce_CoercesDouble()
+        {
+            string toCoerce = "1.1";
+
+            var result = coercer.Coerce(toCoerce);
+
+            Assert.IsInstanceOfType(result, typeof(double));
+        }
+
+        [TestMethod]
+        public void TypeCoercer_Coerce_CoercesInt()
+        {
+            string toCoerce = "1";
+
+            var result = coercer.Coerce(toCoerce);
+
+            Assert.IsInstanceOfType(result, typeof(int));
+        }
+
+        [TestMethod]
+        public void TypeCoercer_Coerce_CoercesLong()
+        {
+            string toCoerce = ((long) int.MaxValue + 1).ToString();
+
+            var result = coercer.Coerce(toCoerce);
+
+            Assert.IsInstanceOfType(result, typeof(long));
+        }
+
+        [TestMethod]
+        public void TypeCoercer_Coerce_CoercesString()
+        {
+            string toCoerce = "foo";
+
+            var result = coercer.Coerce(toCoerce);
+
+            Assert.IsInstanceOfType(result, typeof(string));
+        }
+    }
+}
